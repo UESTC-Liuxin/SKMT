f8455efde03b2c7bddfb015245d7c73d5f526bf9
UESTC-Liuxin <625092890@qq.com>
2020-11-05 10:47:52 +0800

    update .gitignore

--- mmsegmentation/mmseg/models/backbones/__init__.py
+++
@@ -1,5 +0,0 @@
-from .hrnet import HRNet
-from .resnet import ResNet, ResNetV1c, ResNetV1d
-from .resnext import ResNeXt
-
-__all__ = ['ResNet', 'ResNetV1c', 'ResNetV1d', 'ResNeXt', 'HRNet']
--- mmsegmentation/mmseg/models/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .res_layer import ResLayer
-from .self_attention_block import SelfAttentionBlock
-
-__all__ = ['ResLayer', 'SelfAttentionBlock']
